63ea4111a176db2d72af4fd956b0217d1b4b1610
tilakpatidar <tilakpatidar@gmail.com>
2017-07-27 16:53:37 +0530

    Add before build script

---
+++ goblin/before_build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# This will only download archive if it does not exists -nc
+wget -nc https://github.com/apache/incubator-gobblin/releases/download/gobblin_0.10.0/gobblin-distribution-0.10.0.tar.gz
