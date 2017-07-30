4b5b63c2aa263951715266910f8efbc2650de667
tilakpatidar <tilakpatidar@gmail.com>
2017-07-30 20:20:41 +0530

    Add a before build script for hive metastore

---
+++ hive-metastore/before_build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+REPOSRC=https://github.com/gruppopam/lake_garda.git
+LOCALREPO=lake_garda
+
+LOCALREPO_VC_DIR=$LOCALREPO/.git
+
+if [ ! -d $LOCALREPO_VC_DIR ]
+then
+    git clone $REPOSRC $LOCALREPO
+else
+    (cd $LOCALREPO && git pull $REPOSRC)
+fi
+
+mkdir -p hive_migrations
+cp lake_garda/migrations/src/main/db/migrations/*.sql ../hive_migrations/
+rm -rf ../hive_migrations/.migration_info
